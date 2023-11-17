2179d33d7c84811fa45f32ce1a61d764ffd23cc8
Diego <diegoborjas@gmail.com>
2023-11-16 20:39:54 -0600

    update for heroku

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
     server: {
-    port: process.env.PORT || 3000,
+    port: 3000,
     open: true,
     proxy: {
       '/graphql': {
-        target: 'https://my-restaurant-app-d7f65203a1b2.herokuapp.com/' || 'http://localhost:3001',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
